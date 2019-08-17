aaf5c0d64e9d3f7895201d17d016e1a0ed849ffa
puchi2000 <diegovelazqu@hotmail.com>
2019-08-16 20:46:42 -0500

    Ya se termino el último métoddo

--- Practica1/Practica1.hs
+++ Practica1/Practica1.hs
@@ -15,11 +15,28 @@ comparar3 (a, b, c) = if(a<=b) then (if(a <=c) then a else c) else (if(b<=c) the
 isPar :: (Int, Int, Int, Int) -> Bool
 isPar (a,b,c,d) = (mod a 2 == 0) && (mod b 2== 0) && (mod c 2==0) && (mod d 2==0)
 
+-- Determina el número de veces que se repiten los elementos dentro de una lista 
 count :: [Int] -> [(Int, Int)]
 count [] = []
-count (x:xs) = [(x, contar x (x:xs))]++ count xs
+count (x:xs) = elimina([(x, contar x (x:xs))]++ count xs)
 
+-- Cuenta las veces que se repite un elemento en una lista
 
 contar :: Int -> [Int] -> Int
 contar a [] = 0
 contar a (x:xs) = if(a == x) then 1 + (contar a xs) else 0 + (contar a xs)
+
+
+
+-- Elimina una tupla con el elemento fst repetido en una lista de tuplas
+
+eliminaAux :: (Int, Int) -> [(Int, Int)] -> [(Int, Int)]
+eliminaAux a [] = []
+eliminaAux a (x:xs) = if(fst a /= fst x) then [x]++ eliminaAux a xs  else eliminaAux a xs
+
+eliminaAuxAbstract :: [(Int, Int)] -> [(Int, Int)]
+eliminaAuxAbstract (x:xs) = (eliminaAux x xs)
+
+elimina :: [(Int, Int)] -> [(Int, Int)]
+elimina [] = []
+elimina (x:xs)= [x]++ elimina(eliminaAuxAbstract (x:xs))
